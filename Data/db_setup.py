55292797e49aa6c875bdee32ad3ed6bfcf8e24f4
janhrastnik <jan.hrastnik2@gmail.com>
2025-08-05 11:17:49 +0200

    premaknil setup v svoj class

--- Data/db_setup.py
+++ Data/db_setup.py
@@ -2,12 +2,12 @@ import psycopg2
 import sys
 import os
 import random
-from auth import *
+from Data.auth import auth
 
 """
-This script sets up the database if the user doesn't have the 'nlp' 
+This class sets up the database if the user doesn't have the 'nlp' 
 database yet. We expect this to run when the user launches the app 
-for the first time. 
+for the first time.
 
 We create required tables and fill them with data, that
 we got from https://www.kaggle.com/datasets/NUFORC/ufo-sightings/data.
@@ -15,161 +15,190 @@ we got from https://www.kaggle.com/datasets/NUFORC/ufo-sightings/data.
 TODO: maybe add more placeholder data here, like other users?
 """
 
-conn = psycopg2.connect(dbname=auth['db'], user=auth['user'], password=auth['password'], host = auth['host'], port=auth['port'])
+class DatabaseSetup:
+    def __init__(self):
+        self.conn = psycopg2.connect(dbname=auth['db'], user=auth['user'], password=auth['password'], host = auth['host'], port=auth['port'])
+        self.cur = self.conn.cursor()
 
-cur = conn.cursor()
+    def should_setup(self) -> bool:
+        # we determine if we need to run the setup process
+        # obviously we don't want to run the setup if the user already has a filled database created
 
-args = sys.argv
-try:
-    if args[1] == "--clean":
-        # wipe any previous tables that might exist to do a clean setup
+        setup_flag = False
 
-        cur.execute("""
+        # we know that the database already exists, that responsibility falls onto the user
+
+        # check if sightings table exists
+        self.cur.execute("""
+            SELECT 1
+            FROM information_schema.tables
+            WHERE table_schema = 'public'
+              AND table_name = 'sightings';
+            """)
+
+        res_1 = self.cur.fetchone()
+
+        if res_1:
+            # check if sightings table has any entries
+            self.cur.execute("""
+                SELECT count(*) AS exact_count FROM sightings;
+                """)
+
+            res_2 = self.cur.fetchone()
+
+            if res_2[0] != 0:
+                setup_flag = False
+
+        return setup_flag
+
+    def setup(self):
+        self.wipe_any_old_data()
+        self.setup_tables()
+        self.setup_data()
+
+    def wipe_any_old_data(self):
+        self.cur.execute("""
             DROP TABLE IF EXISTS users cascade;
             DROP TABLE IF EXISTS sightings cascade;
             DROP TABLE IF EXISTS comments;
             DROP TABLE IF EXISTS locations;
             DROP TABLE IF EXISTS shapes;
-        """)
-except IndexError:
-    pass
+            """)
 
-# TABELA UPORABNIKOV
-cur.execute("""CREATE TABLE users (
-    id serial PRIMARY KEY,
-    username varchar(50) unique,
-    email varchar(255) unique,
-    password varchar(255),
-    salt text
-);""")
+        self.conn.commit()
 
-# TABELA OPAŽANJ
-cur.execute("""CREATE TABLE sightings (
-    id serial PRIMARY KEY,
-    title varchar(255),
-    sighting_date timestamp,
-    description text,
-    coords point,
-    duration varchar(255),
-    user_id integer not null references users(id) on delete cascade,
-    creation_date timestamp default current_timestamp
-);""")
+    def setup_tables(self):
+        # TABELA UPORABNIKOV
+        self.cur.execute("""CREATE TABLE users (
+            id serial PRIMARY KEY,
+            username varchar(50) unique,
+            email varchar(255) unique,
+            password varchar(255),
+            salt text
+        );""")
 
-# TABELA KOMENTARJEV
-cur.execute("""CREATE TABLE comments (
-    id serial PRIMARY KEY,
-    content text,
-    creation_date timestamp default current_timestamp,
-    user_id integer not null references users(id) on delete cascade,
-    sighting_id integer not null references sightings(id) on delete cascade 
-);""")
+        # TABELA OPAŽANJ
+        self.cur.execute("""CREATE TABLE sightings (
+            id serial PRIMARY KEY,
+            title varchar(255),
+            sighting_date timestamp,
+            description text,
+            coords point,
+            duration varchar(255),
+            user_id integer not null references users(id) on delete cascade,
+            creation_date timestamp default current_timestamp
+        );""")
 
-# TABELA LOKACIJ
-cur.execute("""CREATE TABLE locations (
-    id serial PRIMARY KEY,
-    city varchar(255),
-    country varchar(255)
-);""")
+        # TABELA KOMENTARJEV
+        self.cur.execute("""CREATE TABLE comments (
+            id serial PRIMARY KEY,
+            content text,
+            creation_date timestamp default current_timestamp,
+            user_id integer not null references users(id) on delete cascade,
+            sighting_id integer not null references sightings(id) on delete cascade 
+        );""")
 
-cur.execute("""CREATE TABLE shapes (
-    id serial PRIMARY KEY,
-    name varchar(255)   
-);""")
+        # TABELA LOKACIJ
+        self.cur.execute("""CREATE TABLE locations (
+            id serial PRIMARY KEY,
+            city varchar(255),
+            country varchar(255)
+        );""")
 
-# create some placeholder users
-cur.execute("""INSERT INTO users VALUES (
-    1,
-    'VladimirBober',
-    'vladimir-bober@fake-email',
-    'bad-password',
-    '123'
-);""")
+        # TABELA OBLIK OPAŽANJ
+        self.cur.execute("""CREATE TABLE shapes (
+            id serial PRIMARY KEY,
+            name varchar(255)   
+        );""")
 
-cur.execute("""INSERT INTO users VALUES (
-    2,
-    'JohnSmith',
-    'john-smith@fake-email',
-    'bad-password',
-    '123'
-);""")
+        self.conn.commit()
 
-cur.execute("""INSERT INTO users VALUES (
-    3,
-    'JaneDoe',
-    'jane-doe@fake-email',
-    'bad-password',
-    '123'
-);""")
+    def setup_data(self):
+        # create some placeholder users
+        self.cur.execute("""INSERT INTO users VALUES (
+            1,
+            'VladimirBober',
+            'vladimir-bober@fake-email',
+            'bad-password',
+            '123'
+        );""")
 
-cur.execute("""INSERT INTO users VALUES (
-    4,
-    'WillSmith',
-    'will-smith@fake-email',
-    'bad-password',
-    '123'
-);""")
+        self.cur.execute("""INSERT INTO users VALUES (
+            2,
+            'JohnSmith',
+            'john-smith@fake-email',
+            'bad-password',
+            '123'
+        );""")
 
-cur.execute("""INSERT INTO users VALUES (
-    5,
-    'KevinBaker',
-    'kevin-baker@fake-email',
-    'bad-password',
-    '123'
-);""")
+        self.cur.execute("""INSERT INTO users VALUES (
+            3,
+            'JaneDoe',
+            'jane-doe@fake-email',
+            'bad-password',
+            '123'
+        );""")
 
-# we get the absolute file path of our csv data file, otherwise relative filepath would work only from certain working directory
-# downside is that if we move any of the files, this will silently break
-dirname = os.path.dirname(__file__)
-data_filepath = os.path.join(dirname, '../podatki/complete.csv')
+        self.cur.execute("""INSERT INTO users VALUES (
+            4,
+            'WillSmith',
+            'will-smith@fake-email',
+            'bad-password',
+            '123'
+        );""")
 
-# DATA COLUMNS IN ORDER
-# datetime,city,state,country,shape,duration (seconds),duration (hours/min),comments,date posted,latitude,longitude
+        self.cur.execute("""INSERT INTO users VALUES (
+            5,
+            'KevinBaker',
+            'kevin-baker@fake-email',
+            'bad-password',
+            '123'
+        );""")
 
-# here we add in our existing ufo data we got from kaggle
-# assume, we have some placeholder users already defined in the db
-# and randomly assign them the ufo sightings as if they were theirs
-# for the sake of making a demo project
-with open(data_filepath) as F:
-    lines = [x.strip() for x in F.readlines()]
-    #print(lines[0])
-    #print(lines[1])
-    #print(lines[2])
+        # we get the absolute file path of our csv data file, otherwise relative filepath would work only from certain working directory
+        # downside is that if we move any of the files, this will silently break
+        dirname = os.path.dirname(__file__)
+        data_filepath = os.path.join(dirname, '../podatki/complete.csv')
 
+        # DATA COLUMNS IN ORDER
+        # datetime,city,state,country,shape,duration (seconds),duration (hours/min),comments,date posted,latitude,longitude
 
-    queries = []
+        # here we add in our existing ufo data we got from kaggle
+        # assume, we have some placeholder users already defined in the db
+        # and randomly assign them the ufo sightings as if they were theirs
+        # for the sake of making a demo project
+        with open(data_filepath) as F:
+            lines = [x.strip() for x in F.readlines()]
 
-    for i, line in enumerate(lines[1:]):
-        line = line.split(",")
-        # SIGHTINGS COLUMNS IN ORDER
-        # title, sighting_date, description, coords, duration, user_id, creation_date 
-        shape = "unknown" if line[4] == "" else line[4]
-        title = f"Sighting of {shape}-shaped UFO in {line[1].capitalize()}, {line[2].capitalize()}"
-        coords = f"({line[9]}, {line[10]})"
+            queries = []
 
-        query = f"""INSERT INTO sightings VALUES (
-            {i},
-            '{title}',
-            '{line[0]}',
-            '{line[7]}',
-            '{coords}',
-            '{line[6]}',
-            '{random.randint(1, 5)}',
-            '{line[8]}'
-        )
-        """
+            for i, line in enumerate(lines[1:]):
+                line = line.split(",")
+                # SIGHTINGS COLUMNS IN ORDER
+                # title, sighting_date, description, coords, duration, user_id, creation_date 
+                shape = "unknown" if line[4] == "" else line[4]
+                title = f"Sighting of {shape}-shaped UFO in {line[1].capitalize()}, {line[2].capitalize()}"
+                coords = f"({line[9]}, {line[10]})"
 
-        queries.append(query)
+                query = f"""INSERT INTO sightings VALUES (
+                    {i},
+                    '{title}',
+                    '{line[0]}',
+                    '{line[7]}',
+                    '{coords}',
+                    '{line[6]}',
+                    '{random.randint(1, 5)}',
+                    '{line[8]}'
+                )
+                """
 
-    for query in queries:
-        # print(query)
+                queries.append(query)
 
-        try:
-            cur.execute(query)
-        except psycopg2.errors.InvalidTextRepresentation:
-            # some of the sighting entries are flawed, we should not bother rescuing all of them
-            conn.rollback()
+            for query in queries:
+                try:
+                    self.cur.execute(query)
+                except psycopg2.errors.InvalidTextRepresentation:
+                    # some of the sighting entries are flawed, we should not bother rescuing all of them
+                    self.conn.rollback()
 
-conn.commit()
-
-cur.close()
-conn.close()
+        self.conn.commit()
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from bottle import run, route, template, get
 from bottle import TEMPLATE_PATH
 from Services.sighting_service import SightingService
+from Data.db_setup import DatabaseSetup
 
 TEMPLATE_PATH.append('./Presentation/views')
 
 sighting_service = SightingService()
+database_setup = DatabaseSetup()
 
 @route('/hello')
 def hello():
@@ -24,5 +26,8 @@ def get_all_sightings():
     sighting_service.get_all_sightings()
 
 if __name__ == '__main__':
+    # TODO: use sys.argv to enable user to force a clean setup
+    if database_setup.should_setup():
+        database_setup.setup()
     run(host='localhost', port = 8080, debug=True)
 
